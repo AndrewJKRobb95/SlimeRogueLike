060c1ae379f755af9466f4bac6e98435b1cad4a8
JeremyPlott <54282560+JeremyPlott@users.noreply.github.com>
2020-01-18 21:53:11 -0500

    added main menu. idk if gitignore is working or not, looks liek not

---
+++ SlimeRogueLike/Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject settingsPanel;
+
+    public void StartGame()
+    {
+        //may replace the scene index with string names later, depending on ease and if Unity scene PKs reassign themselves based on order or not
+        Debug.Log("Starting game...");
+        SceneManager.LoadScene(1);
+    }
+
+    public void SettingsMenuToggle()
+    {
+        settingsPanel.SetActive(!settingsPanel.activeSelf);
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("Ending game...");
+        Application.Quit();
+    }
+}
